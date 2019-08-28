f9e40e1f99897beb6b6ca828a88685ee1d5c9548
Malind Kowalski <malind.nikulski@gmail.com>
2019-08-28 11:09:13 -0600

    import GET SONGS SUCCESS and begin dispatch

--- src/actions/songs.js
+++ src/actions/songs.js
@@ -1,10 +1,11 @@
 import api from '../api/api';
+import { GET_SONGS_SUCCESS } from './reducers/songs';
 
 export const getSongs = () => {
   return dispatch => {
     return api.get('/songs')
     .then(songs => {
-
+      dispatch({ type: GET_SONGS_SUCCESS, payload: response.data })
     })
   }
 }
