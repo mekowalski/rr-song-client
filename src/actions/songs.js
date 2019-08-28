24bc32fc66b186df43690ae9b86e62250b6b3e21
Malind Kowalski <malind.nikulski@gmail.com>
2019-08-28 13:45:51 -0600

    go back to first setup, payload is songs

--- src/actions/songs.js
+++ src/actions/songs.js
@@ -1,22 +1,23 @@
 import api from '../api/api';
 import GET_SONGS_SUCCESS  from '../reducers/songs';
 
-// export const getSongs = () => {
-//   return dispatch => {
-//     return api.get('/songs')
-//     .then(songs => {
-//       dispatch({ type: GET_SONGS_SUCCESS, payload: response.data })
-//     })
-//   }
-// }
+export const getSongs = () => {
+  return dispatch => {
+    return api.get('/songs')
+    .then(songs => {
+      dispatch({ type: GET_SONGS_SUCCESS, payload: songs })
+      console.log(songs)
+    })
+  }
+}
 
 //how to get response from api/api.js
 
-export const getSongs = () => {
-  return dispatch => {
-    const response = api.get('/songs')
-
-    dispatch({ type: GET_SONGS_SUCCESS, payload: response.data })
-    console.log(response) //this does log the promise retuens with the 2 songs
-  }
-}
+// export const getSongs = () => {
+//   return dispatch => {
+//     const response = api.get('/songs')
+//
+//     dispatch({ type: GET_SONGS_SUCCESS, payload: response.data })
+//     // console.log(response) //this does log the promise retuens with the 2 songs
+//   }
+// }
